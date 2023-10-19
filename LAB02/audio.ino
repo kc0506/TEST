4cde766204876ae2c4acff53207dd93447b6c9ca
ZoeTsou <zoe.tsou.2003@gmail.com>
2023-10-19 12:28:01 +0800

    LED parts completed

--- LAB02/audio.ino
+++ LAB02/audio.ino
@@ -1,23 +1,38 @@
 #define button 7
 #define volume A4
+#define LED 13
 
 bool prev_b = 0;
 int prev_v = 0;
+int scale = 100;
+
+bool music_state = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(button, INPUT);
   pinMode(volume, INPUT);
-  pinMode(13, OUTPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   bool b = digitalRead(button);
-  int v = analogRead(volume);
-  if (b) digitalWrite(13, HIGH);
-  else digitalWrite(13, LOW);
+  int v = analogRead(volume); // [0, 1023]
+
+  if (prev_b != b && b){
+    music_state = !music_state;
+  }
+
+  if (music_state){
+    digitalWrite(LED, HIGH);
+    delay(v/scale);
+    digitalWrite(LED, LOW);
+    delay((1023-v)/scale);
+  } else{
+    digitalWrite(LED, LOW);
+  }
   
   if(prev_b != b && b){
     Serial.print("b\n");
